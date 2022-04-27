b601d8c87a038642f66d7a48428c6244aa9b87fd
Dawid Bogon <bogon.dawid@gmail.com>
2022-04-27 22:41:19 +0200

    cosmetic changes

--- src/findslot/find-available-slot.py
+++ src/findslot/find-available-slot.py
@@ -6,23 +6,25 @@ from datetime import datetime, timedelta
 import pandas as pd
 
 
-def find_avalible_slot(calendars, dur_in_mins, min_people):
+def find_avalible_slot(calendars:str, dur_in_mins: int, min_people: int) -> str:
     def path_leaf(path):
         head, tail = ntpath.split(path)
         return tail or ntpath.basename(head)
 
     cur_time = datetime.now()
     dur_in_mins_dt_object = timedelta(minutes=dur_in_mins)
-    # im skipping first element here because its not needed, at least on Windows, im not
+    # im skipping first element here because its not needed,
+    # at least on Windows, im not
     # sure if it will work on linux like this
     # paths to text files
-    cal_paths = glob.glob(calendars[1:]+r'\*.txt')
-    peoples_names = [path_leaf(path)[:-4] for path in cal_paths]
+    cal_paths: list = glob.glob(calendars[1:]+r'\*.txt')
+    peoples_names: list = [path_leaf(path)[:-4] for path in cal_paths]
 
     # stores busy times for each person:
-    busy_dict = {}
-    # stores all timestamps, and times 1 second befeor start of being busy and 1 second after not being busy.
-    timestamps = []
+    busy_dict: dict = {}
+    # stores all timestamps, and times 1 second befeor start
+    #  of being busy and 1 second after not being busy.
+    timestamps: list = []
 
     for idx, file in enumerate(cal_paths):
         # name_person = path_leaf(file)[:-4]
@@ -31,10 +33,13 @@ def find_avalible_slot(calendars, dur_in_mins, min_people):
             for line in fil:
                 if len(line) == 11:
                     start_date = datetime.strptime(line[0:10], '%Y-%m-%d')
-                    end_date = start_date + timedelta(hours=23, minutes=59, seconds=59)
+                    end_date = start_date + timedelta(
+                                hours=23, minutes=59, seconds=59)
                 else:
-                    start_date = datetime.strptime(line[:19], '%Y-%m-%d %H:%M:%S')
-                    end_date = datetime.strptime(line[22:], '%Y-%m-%d %H:%M:%S')
+                    start_date = datetime.strptime(
+                                line[:19], '%Y-%m-%d %H:%M:%S')
+                    end_date = datetime.strptime(
+                                line[22:], '%Y-%m-%d %H:%M:%S')
 
                 timestamps.append(start_date)
                 timestamps.append(start_date - timedelta(seconds=1))
@@ -42,14 +47,14 @@ def find_avalible_slot(calendars, dur_in_mins, min_people):
                 timestamps.append(end_date+timedelta(seconds=1))
 
                 busy_dict[peoples_names[idx]].append((start_date, end_date))
-    
+
     timestamps = sorted(set(timestamps))
-    # timestamps.pop(0)
-    # DOPYTAĆ SIĘ O TO czy 30 min to 30 in czy 29 minut i 59 sekund
-    # im adding extra elemnt, it will be end time of meeting if any earlier time is not suitable
+    # im adding extra elemnt, it will be end time of meeting
+    # if any earlier time is not suitable
     timestamps.append(timestamps[-1]+dur_in_mins_dt_object)
-    # timestamps.insert(0,timestamps[0]-dur_in_mins_dt_object)
-    # creating pandas DataFrame with columns dates, and avalibility of each person
+
+    # creating pandas DataFrame with columns dates,
+    # and avalibility of each person
     df = pd.DataFrame(timestamps, columns=["dates"])
     for name in peoples_names:
         df[name] = 1
@@ -61,16 +66,19 @@ def find_avalible_slot(calendars, dur_in_mins, min_people):
                 if start_date <= df["dates"][idx] <= end_date:
                     df.loc[idx, person] = 0
 
-    # setting meeting time in worst case scenario or returning curent time if its later than
+    # setting meeting time in worst case scenario or
+    # returning curent time if its later than
     # last elem in array
     if cur_time > timestamps[-2]:
         return cur_time
     else:
         metting_time = timestamps[-2]
-    
-    # iterating over every combination of people looking for earliest time to start meeting
 
-    # this is unnecesary, but if lets say we would like to check avbability
+    # iterating over every combination of people looking
+    # for earliest time to start meeting
+
+    # this is unnecesary, but if lets say we would like
+    # to check avbability
     # for each combination of persons then this would be needed
     # for n_of_people in range(min_people,len(peoples_names)+1):
 
@@ -106,7 +114,8 @@ if __name__ == "__main__":
         help="Minimum number of people that must be avalible"
     )
     # for debbuging purposes i have added
-    # parser.add_argument("-v",'--verbose',action="store_true",help="print meeting start time and avalible people")
+    # parser.add_argument("-v",'--verbose',action="store_true",
+    # help="print meeting start time and avalible people")
     args = parser.parse_args()
 
     date = find_avalible_slot(args.calendars, args.duration_in_minutes, args.minumum_people)
