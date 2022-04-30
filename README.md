f21928cdfc42fdddced3e0e99d0534a9504342b7
Dawid Bogon <bogon.dawid@gmail.com>
2022-04-30 13:36:11 +0200

    fixed tests

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Recruitment task for OpenX Deals/API Intern
 It was my first time working with pytest so the tests are not best. I tried installing freeztime in order to test it properly but it did not work. Next thing i wanted to do is to test it in diffrent enviroments using tox and run tests on github automatically.
 
 
-In order to run tests simply execute commad(You have to be in parent directory of this repository, from folder openX_task2 run command cd ../):
+In order to run tests simply from folder openX_task2 execute commad:
 
 ``` pytest```
 
--- tests/test_find_avalible_slot.py
+++ tests/test_find_avalible_slot.py
@@ -4,18 +4,15 @@ import pytest
 from ..src.findslot import *
 # from freezegun import freeze_time
 
-def test_print():
-    assert 'hello' == "hello"
-
 # @freeze_time("2022-07-01 09:00:00")
 def test1():
-    assert findslot.find_avalible_slot('/openX_task2/in', 30,2) == '2022-07-02 00:00:00'
+    assert findslot.find_avalible_slot('/in', 30,2) == '2022-07-02 00:00:00'
 
 def test2():
-    assert findslot.find_avalible_slot('/openX_task2/in', 10,2) == '2022-07-02 00:00:00'
+    assert findslot.find_avalible_slot('/in', 10,2) == '2022-07-02 00:00:00'
 
 def test3():
-    assert findslot.find_avalible_slot('/openX_task2/in', 10,3) == '2022-07-02 13:02:00'
+    assert findslot.find_avalible_slot('/in', 10,3) == '2022-07-02 13:02:00'
 
 def test4():
-    assert findslot.find_avalible_slot('/openX_task2/in', 30,3) == '2022-07-02 14:00:00'
+    assert findslot.find_avalible_slot('/in', 30,3) == '2022-07-02 14:00:00'
